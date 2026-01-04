11291fd2879be99b748703073cddb88a982c3669
Chris Tonkinson <chris@tonkinson.com>
2026-01-04 08:28:09 -0500

    Add utility script

---
+++ scripts/publish.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ "$#" -eq 0 ]]; then
+  echo "Usage: $0 <commit message>"
+  exit 1
+fi
+
+message="$*"
+
+git add -A
+git commit -m "${message}"
+git tag -f latest
+git push
+git push -f origin latest
